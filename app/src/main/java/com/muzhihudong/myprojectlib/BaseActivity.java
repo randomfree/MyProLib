82298e94a63960f303d204f9c8a2f0015da73067
louyu <746831652@qq.com>
2016-05-16 14:00:06 +0800

    我的开发支持库，欢迎指导

---
+++ app/src/main/java/com/muzhihudong/myprojectlib/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.muzhihudong.myprojectlib;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+
+public abstract class BaseActivity extends FragmentActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/com/muzhihudong/myprojectlib/MainActivity.java
@@ -0,0 +1,17 @@
+package com.muzhihudong.myprojectlib;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
